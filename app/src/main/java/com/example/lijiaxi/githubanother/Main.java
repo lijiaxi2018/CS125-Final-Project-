b2a6cfa3c5137d4609fe20b0c9d629551b6a76e7
lijiaxi2018 <jiaxili3@illinois.edu>
2018-11-30 21:04:25 -0600

    develop

--- app/src/main/java/com/example/lijiaxi/githubanother/Main.java
+++ app/src/main/java/com/example/lijiaxi/githubanother/Main.java
@@ -3,5 +3,6 @@ package com.example.lijiaxi.githubanother;
 public class Main {
     public static void unused() {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
