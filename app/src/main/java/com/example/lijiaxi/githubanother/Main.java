fa9dc092d42611188b5303590d45810c428f4de6
chenyuzhao <chenyu5@illinois.edu>
2018-11-30 21:44:21 -0600

    Hello delete

--- app/src/main/java/com/example/lijiaxi/githubanother/Main.java
+++ app/src/main/java/com/example/lijiaxi/githubanother/Main.java
@@ -5,8 +5,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        
     }
 }
