a8508373e04df9a323454e53b03b81f14a106fe3
chenyuzhao <chenyu5@illinois.edu>
2018-11-30 21:29:44 -0600

    Hello THIRD

--- app/src/main/java/com/example/lijiaxi/githubanother/Main.java
+++ app/src/main/java/com/example/lijiaxi/githubanother/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void unused() {
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
