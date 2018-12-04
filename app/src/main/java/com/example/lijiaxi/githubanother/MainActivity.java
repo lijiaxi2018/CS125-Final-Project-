e12c0e272d0dcb761616535d0291acb916c11e16
chenyuzhao <chenyu5@illinois.edu>
2018-12-04 09:53:24 -0600

    changed gradle-build-dependency: added volley

--- app/src/main/java/com/example/lijiaxi/githubanother/MainActivity.java
+++ app/src/main/java/com/example/lijiaxi/githubanother/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.lijiaxi.githubanother;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
