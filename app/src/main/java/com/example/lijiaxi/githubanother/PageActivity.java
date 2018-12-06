93068cda593616a6d77c8e84bacfbf70b72685bb
lijiaxi2018 <jiaxili3@illinois.edu>
2018-12-06 17:04:28 -0600

    ljx 12/6 17:00 取消多个activity

--- app/src/main/java/com/example/lijiaxi/githubanother/PageActivity.java
+++ app/src/main/java/com/example/lijiaxi/githubanother/PageActivity.java
@@ -5,58 +5,101 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PageActivity extends AppCompatActivity {
+
+
+    private QuestionLiarbry myQuestions = new QuestionLiarbry();
+
+    private TextView mQuestionView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private Button mChoice3;
+    private Button mChoice4;
+
+    private String mAnswer;
+    private int score = 0;
+    private int questionNumber = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
 
-        right();
-        wrong1();
-        wrong2();
-        wrong3();
+        mQuestionView = (TextView)findViewById(R.id.questionView) ;
+        mChoice1 = (Button)findViewById(R.id.answer1);
+        mChoice2 = (Button)findViewById(R.id.answer2);
+        mChoice3 = (Button)findViewById(R.id.answer_3);
+        mChoice4 = (Button)findViewById(R.id.answer4);
+        updateQuestion();
         back();
-    }
-    private void right() {
-        Button restartButton = (Button) findViewById(R.id.answer1);
-        restartButton.setOnClickListener(new View.OnClickListener() {
+
+
+        if (questionNumber == 2) {
+            startActivity(new Intent(PageActivity.this, FinalActivity.class));
+        }
+
+        mChoice1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PageActivity.this, Page2Activity.class));
+                if (mChoice1.getText() == mAnswer) {
+                    score++;
+                    updateQuestion();
+                } else {
+                    updateQuestion();
+                }
             }
         });
+
+        mChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mChoice2.getText() == mAnswer) {
+                    score++;
+                    updateQuestion();
+                } else {
+                    updateQuestion();
+                }
+            }
+        });
+
+        mChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mChoice3.getText() == mAnswer) {
+                    score++;
+                    updateQuestion();
+                } else {
+                    updateQuestion();
+                }
+            }
+        });
+
+        mChoice4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mChoice4.getText() == mAnswer) {
+                    score++;
+                    updateQuestion();
+                } else {
+                    updateQuestion();
+                }
+            }
+        });
+
     }
 
-    private void wrong1() {
-        Button restartButton = (Button) findViewById(R.id.answer2);
-        restartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(PageActivity.this, FinalActivity.class));
-            }
-        });
-    }
+    private void updateQuestion() {
+        mQuestionView.setText(myQuestions.getQuestion(questionNumber));
+        mChoice1.setText(myQuestions.getChoice1(questionNumber));
+        mChoice2.setText(myQuestions.getChoice2(questionNumber));
+        mChoice3.setText(myQuestions.getChoice3(questionNumber));
+        mChoice4.setText(myQuestions.getChoice4(questionNumber));
+        mAnswer = myQuestions.getCorrect(questionNumber);
 
-    private void wrong2() {
-        Button restartButton = (Button) findViewById(R.id.answer3);
-        restartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(PageActivity.this, FinalActivity.class));
-            }
-        });
-    }
-
-    private void wrong3() {
-        Button restartButton = (Button) findViewById(R.id.answer4);
-        restartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(PageActivity.this, FinalActivity.class));
-            }
-        });
+        questionNumber++;
     }
 
     private void back() {
